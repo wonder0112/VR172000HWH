91b0b9a8d9123657706b0dda71a8c518649dfe59
wonder0112 <hwhgdcn@gmail.com>
2019-03-07 11:48:37 +0800

    广东职业技术学院信息工程系移动172VR项目

---
+++ app/src/main/java/cn/edu/gdpt/xxgcx/vr172000hwh/VRPicActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdpt.xxgcx.vr172000hwh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VRPicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vrpic);
+    }
+}
